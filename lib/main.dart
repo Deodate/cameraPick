f6ac7ab538a6fcca81e776ede3af1a776e0c8acc
Deodate <ajadeo04@gmail.com>
2024-06-23 11:51:00 +0200

    Pick Image is Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:camerapick/upload_profile_image.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 
 void main() {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'IMAGE PICKER',
       theme: ThemeData(
--- lib/upload_profile_image.dart
+++ lib/upload_profile_image.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UploadProfileImage extends StatefulWidget {
   const UploadProfileImage({Key? key}) : super(key: key);
@@ -8,15 +12,152 @@ class UploadProfileImage extends StatefulWidget {
 }
 
 class _UploadProfileImageState extends State<UploadProfileImage> {
+  File? pickedImage; // Stores the picked image file
+
+  // Function to show image picker options in a bottom sheet using GetX
+  void imagePickerOption() {
+    Get.bottomSheet(
+      SingleChildScrollView(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(10.0),
+            topRight: Radius.circular(10.0),
+          ),
+          child: Container(
+            color: Colors.white,
+            height: 250,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const Text(
+                    "Pick Image From",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource
+                          .camera); // Call pickImage with camera source
+                    },
+                    icon: const Icon(Icons.camera),
+                    label: const Text("CAMERA"),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      pickImage(ImageSource
+                          .gallery); // Call pickImage with gallery source
+                    },
+                    icon: const Icon(Icons.image),
+                    label: const Text("GALLERY"),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      Get.back(); // Close bottom sheet on cancel
+                    },
+                    icon: const Icon(Icons.close),
+                    label: const Text("CANCEL"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Function to pick image from camera or gallery based on source
+  pickImage(ImageSource imageType) async {
+    try {
+      final pickedFile = await ImagePicker().pickImage(source: imageType);
+      if (pickedFile == null) return;
+      final tempImage = File(pickedFile.path);
+
+      setState(() {
+        pickedImage = tempImage; // Update pickedImage state with selected image
+      });
+
+      Get.back(); // Close bottom sheet after picking an image
+    } catch (error) {
+      debugPrint(error.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text('UPLOAD PROFILE IMAGE'),
+        title: const Text('IMAGE PICKER'),
       ),
-      body: const Center(
-        child: Text('Example of upload'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Stack(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.indigo, width: 5),
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(100),
+                    ),
+                  ),
+                  child: ClipOval(
+                    child: pickedImage != null
+                        ? Image.file(
+                            pickedImage!,
+                            width: 170,
+                            height: 170,
+                            fit: BoxFit.cover,
+                          )
+                        : Image.network(
+                            'https://upload.wikimedia.org/wikipedia/commons/5/5f/Alberto_conversi_profile_pic.jpg',
+                            width: 170,
+                            height: 170,
+                            fit: BoxFit.cover,
+                          ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 5,
+                  child: IconButton(
+                    onPressed: imagePickerOption,
+                    icon: const Icon(
+                      Icons.add_a_photo_outlined,
+                      color: Colors.blue,
+                      size: 30,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton.icon(
+              onPressed: imagePickerOption,
+              icon: const Icon(Icons.add_a_photo_sharp),
+              label: const Text('UPLOAD IMAGE'),
+            ),
+          )
+        ],
       ),
     );
   }
